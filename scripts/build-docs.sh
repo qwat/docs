e39e833dd30e74233f50216c9c86bb76f7503b02
Arnaud Poncet-Montanges <ponceta@users.noreply.github.com>
2022-01-31 08:18:04 +0100

    actions build adaptation
    actions build adaptation

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -1,45 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Exit on error
 set -e
 
-if [ -z $1 ]
-then
-  TAG=master
-else
-  TAG=$1
-fi
-
-sed -i.bak -e "s/%%VERSION%%/$TAG/g" conf.py
-
-TRANSLATIONS_I18N="en de fr it ro"
-
-GNUMAKE="make"
-if [[ "${OSTYPE,,}" =~ bsd* ]] || [[ "$OSTYPE" =~ darwin* ]]; then
-    GNUMAKE="gmake"
-fi
-
-${GNUMAKE} clean
-
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..
+pushd ${DIR}
+make clean
 if [ -f ~/.transifexrc ]; then
-tx pull -a
+make transifex_pull
 fi
-for LANG in $TRANSLATIONS_I18N
-do
-    ${GNUMAKE} -e SPHINXOPTS="-D language='${LANG}'" -e BUILDDIR="_build/html/${LANG}" html
-done
-
-cd _build/html
-
-cat <<EOF > index.html
-<html>
-<head>
-  <meta http-equiv="Refresh" content="0; url=en/html" />
-</head>
-<body>
-  <p>You will be redirected to the English documentation</p>
-</body>
-</html>
-EOF
-
-touch .nojekyll
+make html
+# make all-pdf
+popd
