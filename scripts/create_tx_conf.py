363f9938b864ded40cd36ee0c52815104cfa3ace
why-not-try-calmer <mrnycticorax@gmail.com>
2023-01-30 10:31:51 +0100

    - simplified CI business logic - reordered push->pull to pull->push to have remote drive updates

--- scripts/create_tx_conf.py
+++ scripts/create_tx_conf.py
@@ -4,23 +4,33 @@
 # Will be automatically run and committed via github actions
 
 from pathlib import Path
-import os
-import tempfile
+from os import environ
 
-repo_slug = 'qwat-doc'
+organization = "qwat"
+project = "qwat-doc"
 
-with open('.tx/config', 'w') as f:
-    f.write('[main]\n')
-    f.write('host = https://www.transifex.com\n\n')
+token = environ.get("TX_TOKEN")
+probe = environ.get("PROBE")
+print(f"Probe reads: {probe}")
+if not token or len(token) < 5:
+    raise ValueError(
+        f"Your TX_TOKEN could not be retrieved from the environment or is too short: (***{token[-3:]})"
+    )
 
-    for pot in Path('i18n/pot/').rglob('*.pot'):
-        rel_pot = pot.relative_to('i18n/pot/')
+with open(".tx/config", "w") as f:
+    config = "[main]\n"
+    config += "host = https://www.transifex.com\n\n"
+
+    for pot in Path("i18n/pot/").rglob("*.pot"):
+        rel_pot = pot.relative_to("i18n/pot/")
         full_stem = rel_pot.parent.joinpath(rel_pot.stem)
-        normalized_name = str(full_stem).replace('_', '-').replace('/', '-')
-        f.write(f'[{repo_slug}.{normalized_name}]\n')
-        f.write(f'file_filter = i18n/<lang>/{full_stem}.po\n')
-        f.write(f'source_file = {pot}\n')
-        f.write('source_lang = en\n')
-        f.write('type = PO\n\n')
+        resource_slug = str(full_stem).replace("_", "-").replace("/", "-")
+        config += f"[o:{organization}:p:{project}:r:{resource_slug}]\n"
+        config += f"file_filter = i18n/<lang>/{full_stem}.po\n"
+        config += f"source_file = {pot}\n"
+        config += "source_lang = en\n"
+        config += "type = PO\n\n"
 
-
+    f.write(config)
+    f.write("\n")
+    print(f"*create_tx_conf.py*: Config updated to: \n---\n{config}")
